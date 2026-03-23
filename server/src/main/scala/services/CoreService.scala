76d18be652589aa33ed8103c15212ec9712840b1
Alec Dorrington <alecdorrington@gmail.com>
2026-03-23 11:15:01 +0100

    Removed unused imports.

--- server/src/main/scala/services/CoreService.scala
+++ server/src/main/scala/services/CoreService.scala
@@ -3,9 +3,7 @@ package services
 
 import cats.effect.IO
 import io.github.sgtswagrid.assetloader.tapir.AssetService
-import sttp.model.StatusCode
 import your_name.project_name.server.api.CoreApi
-import your_name.project_name.server.api.CoreApi.health
 import your_name.project_name.server.config.Env
 import your_name.project_name.server.html.Template
 
